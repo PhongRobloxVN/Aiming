b83608d75b3ec474def9a4b7916bc87edb01f673
PhongRobloxVN <83484488+PhongRobloxVN@users.noreply.github.com>
2022-01-26 11:36:18 +0700

    Update Module.lua

--- Module.lua
+++ Module.lua
@@ -1,8 +1,8 @@
 if getgenv().Aiming then return getgenv().Aiming end
 
 -- // Dependencies
-local SignalManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Signal/main/Manager.lua"))()
-local BeizerManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Aiming/main/BeizerManager.lua"))()
+local SignalManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/PhongRobloxVN/Signal/main/Manager.lua"))()
+local BeizerManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/PhongRobloxVN/Aiming/main/BeizerManager.lua"))()
 
 -- // Services
 local Players = game:GetService("Players")
